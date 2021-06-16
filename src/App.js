3dbfbad0308de5f3e4abdf81c5931428464b5860
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-16 15:41:24 -0400

    got nav bar functional.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Bio from "./Components/bio"
 import Footer from "./Components/footer"
 import './App.css';
+import Navigation from "./Components/nav"
 
 function App() {
   return (
     <div className="App">
+     <Navigation />
      <Bio />
      <Footer />
+     
     </div>
   );
 }
