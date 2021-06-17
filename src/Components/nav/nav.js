76c096a3124ac83a84aa4f012821c0b83f740c39
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-17 14:45:38 -0400

    finished nav bar. finalizing style before routes,

--- src/Components/nav/nav.js
+++ src/Components/nav/nav.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 // import { Navbar,Nav,NavDropdown } from 'react-bootstrap';
 import { MenuItems } from './menuitems';
+import "./navbar.css"
 
 class Navbar extends Component {
+    
     render(){
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React</h1>
+                <h1 className="navbar-logo">Portfolio</h1>
                 <div className="menu-icon">
                     
                 </div>
-                <ul>
+                <ul className="nav-menu">
                     {MenuItems.map((item, index) => {
                         return (
                     <li key={index}>
